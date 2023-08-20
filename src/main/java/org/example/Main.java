d177257ce80144bcfa444791a981d88217503efb
ANIRAV KAREDDY <aniravkareddy@gmail.com>
2023-08-20 11:22:45 -0400

    Third Commit. Added the maven dependency for the OpenAI-Java which I will be incorporating in this project to help with generating the summary of the scraped article, the theme, and more to come.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,12 @@ import org.jsoup.nodes.Element;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,12 +30,48 @@ public class Main {
     private static String scrapeUrlText(String url) {
         try {
             Document document= Jsoup.connect(url).get();
-            Element article=document.select("article").first();
+            Element article=document.select("div.article-content").first();
             return article.text();
         }catch(Exception e) {
             System.err.println("An error occurred: "+e.getMessage());
             return "";
         }
+    }
+    private static String scrapeUrlDate(String url) {
+        try {
+            Document document=Jsoup.connect(url).get();
+            Element article=document.select("span.date").first();
+            return article.text();
+        }catch(Exception e) {
+            System.err.println("An error occurred: "+e.getMessage());
+            return "";
+        }
+    }
+    private static String scraperUrlAuthor(String url) {
+        try {
+            Document document=Jsoup.connect(url).get();
+            Element article=document.selectFirst("span.author");
+            return article.text();
+        }catch(Exception e) {
+            System.err.println("An error occurred: "+e.getMessage());
+            return "";
+
+
+        }
+    }
+    private static String scraperUrlHost(String url) {
+        try {
+            Document document=Jsoup.connect(url).get();
+            java.net.URL javaUrl = new java.net.URL(url);
+            return javaUrl.getHost();
+
+        }catch(Exception e) {
+            System.err.println(e.getMessage());
+            return "";
+        }
+    }
+    private static String gptAnswer(String articleText) {
+        
     }
 
 
